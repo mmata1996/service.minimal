8dc399a114072c5844f06db80c1477416500d935
Miguel Mata <mmata@altice.com.do>
2025-05-12 11:21:45 -0400

    agregando modelo

---
+++ service.minimal.api/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace service.minimal.api.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsComplete { get; set; } = false;
+    }
+}
