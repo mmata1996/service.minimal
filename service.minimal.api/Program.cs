8a5e49f2f112faeff219f0d27957495a7681a30c
Miguel Mata <mmata@altice.com.do>
2025-05-16 16:33:49 -0400

    agregando ejemplos

--- service.minimal.api/Program.cs
+++ service.minimal.api/Program.cs
@@ -1,8 +1,14 @@
-using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using service.minimal.api.Models;
 using service.minimal.api.Repositories.Contracts;
 using service.minimal.api.Repositories.Implentations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace service.minimal.api
 {
@@ -12,12 +18,61 @@ namespace service.minimal.api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddAuthorization();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    //url de quien emite el token   
+                    ValidIssuer = "http://localhost:5238",
+                    //user que puede usar el token
+                    ValidAudience = "http://localhost:5238",
+                    RequireExpirationTime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes("Tu_Clave_Secreta_Larga_Y_Segura_123456!")
+                    )
+                };
+            });
+
+
+            builder.Services.AddAuthorizationBuilder();
+
+            builder.Services.AddAuthorizationBuilder();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(setup =>
+            {
+                var jwtSecurityScheme = new OpenApiSecurityScheme
+                {
+                    BearerFormat = "JWT",
+                    Name = "JWT Authentication",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                    Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+                setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { 
+                        jwtSecurityScheme, Array.Empty<string>() 
+                    }
+                });
+            });
+
             builder.Services.AddSingleton(new List<TodoItem>());
             builder.Services.AddScoped<ITodoItemRepository, TodoItemRepository>();
             var app = builder.Build();
@@ -29,12 +84,14 @@ namespace service.minimal.api
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
 
+            app.UseCors();
+            app.UseAuthentication();
             app.UseAuthorization();
 
             //Middleware para logear las peticiones
-            app.Use((context, next) => { 
+            app.Use((context, next) =>
+            {
                 Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
                 return next(context);
             });
@@ -107,6 +164,216 @@ namespace service.minimal.api
 
                 return result;
             }).WithName("Test");
+
+
+            app.MapGet("/token/get/admin-token", () =>
+            {
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Tu_Clave_Secreta_Larga_Y_Segura_123456!"));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, "mmata@gmail.com"),
+                    new Claim(JwtRegisteredClaimNames.Email, "mmata@gmail.com"),
+                    new Claim("role", "admin"),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                var token = new JwtSecurityToken(
+                    issuer: "http://localhost:5238",
+                    audience: "http://localhost:5238",
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(30),
+                    signingCredentials: creds
+                );
+
+                return new JwtSecurityTokenHandler().WriteToken(token);
+
+            }).WithDisplayName("Generar un Token")
+            .WithSummary("Generar un Token JWT")
+            .WithDescription("Generar un Token JWT para fines de prueba")
+            .WithOpenApi();
+
+            app.MapGet("/token/get/user-token", () =>
+            {
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Tu_Clave_Secreta_Larga_Y_Segura_123456!"));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, "mmata@gmail.com"),
+                    new Claim(JwtRegisteredClaimNames.Email, "mmata@gmail.com"),
+                    new Claim("role", "user"),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                var token = new JwtSecurityToken(
+                    issuer: "http://localhost:5238",
+                    audience: "http://localhost:5238",
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(30),
+                    signingCredentials: creds
+                );
+
+                return new JwtSecurityTokenHandler().WriteToken(token);
+
+            }).WithDisplayName("Generar un Token")
+            .WithSummary("Generar un Token JWT")
+            .WithDescription("Generar un Token JWT para fines de prueba")
+            .WithOpenApi();
+
+
+            app.MapGet("/token/get/admin-user-token", () =>
+            {
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Tu_Clave_Secreta_Larga_Y_Segura_123456!"));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, "mmata@gmail.com"),
+                    new Claim(JwtRegisteredClaimNames.Email, "mmata@gmail.com"),
+                    new Claim("role", "user"),
+                    new Claim("role", "admin"),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                var token = new JwtSecurityToken(
+                    issuer: "http://localhost:5238",
+                    audience: "http://localhost:5238",
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(30),
+                    signingCredentials: creds
+                );
+
+                return new JwtSecurityTokenHandler().WriteToken(token);
+
+            }).WithDisplayName("Generar un Token")
+            .WithSummary("Generar un Token JWT")
+            .WithDescription("Generar un Token JWT para fines de prueba")
+            .WithOpenApi();
+
+            app.MapGet("/token/protegido-admin-no-police-name", () => {
+                return "Token admin valido";
+            })
+            .AddEndpointFilter(async (context, next) => {
+
+                var user = context.HttpContext.User;
+                var identity = user.Identity as ClaimsIdentity;
+
+                if (!identity.IsAuthenticated)
+                {
+                    return Results.Unauthorized();
+                }
+
+                return await next(context);
+            })
+            .WithDisplayName("Endpoint protegido")
+            .WithSummary("Endpoint protegido")
+            .WithDescription("Endpoint protegido")
+            .WithOpenApi();
+
+            app.MapGet("/token/protegido-admin", () => { 
+                return "Token admin valido";
+            })
+            .AddEndpointFilter(async (context, next) =>
+            {
+                var user = context.HttpContext.User;
+
+                if (user == null)
+                    return Results.Unauthorized();
+
+                var identity = user.Identity as ClaimsIdentity;
+
+                if(!identity.IsAuthenticated)
+                    return Results.Unauthorized();
+
+                var haveRole = identity.Claims
+                .Where(x => x.Type.Contains("role"))
+                .Select(x => x.Value);
+
+                var haveAdminrole = haveRole.Where(x => x == "admin").Any();
+
+                if (!haveAdminrole)
+                    return Results.Forbid(null, ["Solo admin, manin"]);
+
+                return await next(context);
+            })
+            .WithDisplayName("Endpoint protegido")
+            .WithSummary("Endpoint protegido")
+            .WithDescription("Endpoint protegido")
+            .WithOpenApi();
+
+            app.MapGet("/token/protegido-user", () => {
+                return "Token user valido";
+            }).AddEndpointFilter(async (context, next) =>
+            {
+                var user = context.HttpContext.User;
+
+                if (user == null)
+                    return Results.Unauthorized();
+
+                var identity = user.Identity as ClaimsIdentity;
+
+                if (!identity.IsAuthenticated)
+                    return Results.Unauthorized();
+
+                var haveRole = identity.Claims
+                .Where(x => x.Type.Contains("role"))
+                .Select(x => x.Value);
+
+                var haveAdminrole = haveRole.Where(x => x == "user").Any();
+
+                if (!haveAdminrole)
+                    return Results.Forbid(null, ["Solo user, manin"]);
+
+                return await next(context);
+            })
+            .WithDisplayName("Endpoint protegido")
+            .WithSummary("Endpoint protegido")
+            .WithDescription("Endpoint protegido")
+            .WithOpenApi();
+
+            app.MapGet("/token/protegido-admin-user", () => {
+                return "Token admin user valido";
+            }).AddEndpointFilter(async (context, next) =>
+            {
+                var user = context.HttpContext.User;
+
+                if (user == null)
+                    return Results.Unauthorized();
+
+                var identity = user.Identity as ClaimsIdentity;
+
+                if (!identity.IsAuthenticated)
+                    return Results.Unauthorized();
+
+                var haveRole = identity.Claims
+                .Where(x => x.Type.Contains("role"))
+                .Select(x => x.Value);
+
+                var haveAdminrole = haveRole.Where(x => x == "admin" || x == "user").Any();
+
+                if (!haveAdminrole)
+                    return Results.Forbid(null, ["Solo admin o user, manin"]);
+
+                return await next(context);
+            })
+            .RequireAuthorization("UserOrAdmin")
+            .WithDisplayName("Endpoint protegido")
+            .WithSummary("Endpoint protegido")
+            .WithDescription("Endpoint protegido")
+            .WithOpenApi();
+
+            app.MapGet("/token/noprotegido", () => {
+                return "no protegido valido";
+            })
+            .WithDisplayName("Endpoint no protegido")
+            .WithSummary("Endpoint no protegido")
+            .WithDescription("Endpoint no protegido")
+            .WithOpenApi();
 
             app.Run();
         }
